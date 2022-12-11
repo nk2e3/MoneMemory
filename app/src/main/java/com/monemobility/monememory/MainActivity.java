c97b55ae9eb9aa66c09778c7aad9a53547427c7f
ckyoshi <ckyoshi@cpp.edu>
2022-12-11 01:21:35 -0800

    Update MainActivity.java

--- app/src/main/java/com/monemobility/monememory/MainActivity.java
+++ app/src/main/java/com/monemobility/monememory/MainActivity.java
@@ -4,9 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
