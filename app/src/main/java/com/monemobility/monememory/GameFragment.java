cfcddbce79683a3f1cd3cba6735cafd97cd1a532
Noah King <ncking@cpp.edu>
2022-12-12 23:14:00 -0800

    finished game

--- app/src/main/java/com/monemobility/monememory/GameFragment.java
+++ app/src/main/java/com/monemobility/monememory/GameFragment.java
@@ -1,26 +1,27 @@
 package com.monemobility.monememory;
 
+import static android.content.Context.MODE_PRIVATE;
 import static android.widget.ImageView.ScaleType.CENTER_INSIDE;
-
-import android.content.Context;
-import android.content.Intent;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
 import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.Arrays;
+import java.util.Collections;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -40,13 +41,19 @@ public class GameFragment extends Fragment {
     int firstCard, secondCard;
     int fcx1, fcy1, fcx2, fcy2;
     int[][] gridImageValues;
-    boolean[][] isCorrect;
     String[][] isCorrectS;
     TextView scoreText;
     boolean revealed;
-    boolean flag;
-    int fragScore = 1;
+
+
+    int lastScore, hs1, hs2, hs3;
+    String lastName, ns1, ns2, ns3;
+    String scoreReadout;
+    int fragScore = 0;
     private int g = 1;
+
+    public static String userName;
+    public String[] hsNames = new String[3];
 
 
     // TODO: Rename and change types of parameters
@@ -110,6 +117,16 @@ public class GameFragment extends Fragment {
                 }
             }
         }
+        loadData();
+
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences(getSharedPrefs(numOfCards), MODE_PRIVATE);
+        //SharedPreferences.Editor editor = sharedPreferences.edit();
+        String init = sharedPreferences.getString("scoreString", scoreReadout);
+
+
+
+
+        Toast.makeText(getContext(), "Press on the Score to see the highscores for this game.", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -170,27 +187,24 @@ public class GameFragment extends Fragment {
                 im[m][n].setTag(Integer.toString(gridImageValues[m][n]));
                 int finalM = m;
                 int finalN = n;
-                //int fs = fragScore;
+
                 im[m][n].setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         im[finalM][finalN].setImageResource(gridImageValues[finalM][finalN]);
                         im[finalM][finalN].setEnabled(false);
-                        //isCorrectS[finalM][finalN] = "true";
 
-                        int x;
+
                         if(g % 2 == 0) {
                             secondCard = Integer.parseInt((String) im[finalM][finalN].getTag());
                             fcx1 = finalM;
                             fcy1= finalN;
-                            x = 2;
+
                         } else {
                             firstCard = Integer.parseInt((String) im[finalM][finalN].getTag());
                             fcx2 = finalM;
                             fcy2 = finalN;
-                            x= 1;
                         }
-                        String eq;
 
 
                         if(g%2 == 0) {
@@ -220,29 +234,26 @@ public class GameFragment extends Fragment {
                                     }
                                 }, 1000);
                             } else if (secondCard != firstCard && !((finalM == fcx2) && (finalN == fcy2))) {
-                                fragScore -= 1;
-                                if(fragScore == 0) {
-                                    revealCards(false);
-                                } else {
-                                    Log.i("STATE","!eq");
-                                    Handler handler = new Handler();
+                                if(fragScore > 0) {
+                                    fragScore -= 1;
+                                }
+                                Log.i("STATE","!eq");
+                                Handler handler = new Handler();
 
-                                    handler.postDelayed(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            isCorrectS[finalM][finalN] = "false";
-                                            isCorrectS[fcx2][fcy2] = "false";
-                                            im[finalM][finalN].setImageResource(R.drawable.ic_go);
-                                            im[fcx2][fcy2].setImageResource(R.drawable.ic_go);
-                                            im[fcx2][fcy2].setEnabled(true);
-                                            im[finalM][finalN].setEnabled(true);
-                                            //disable buttons until timer is done...
-                                        }
-                                    }, 1000);
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        isCorrectS[finalM][finalN] = "false";
+                                        isCorrectS[fcx2][fcy2] = "false";
+                                        im[finalM][finalN].setImageResource(R.drawable.ic_go);
+                                        im[fcx2][fcy2].setImageResource(R.drawable.ic_go);
+                                        im[fcx2][fcy2].setEnabled(true);
+                                        im[finalM][finalN].setEnabled(true);
+                                        //disable buttons until timer is done...
+                                    }
+                                }, 1000);
                                     scoreText = (TextView) getActivity().findViewById(R.id.scoreText);
                                     scoreText.setText("SCORE: " + fragScore);
-                                }
-
                             }
                         }
 
@@ -272,7 +283,6 @@ public class GameFragment extends Fragment {
             }
         } parent.setWeightSum(m_max);
         return view;
-        //return inflater.inflate(R.layout.fragment_game, container, false);
      }
 
     public void revealCards(boolean show) {
@@ -323,16 +333,188 @@ public class GameFragment extends Fragment {
             }
         }
         String[][] solution = new String[m_max][n_max];
-
+        int finalScore = 0;
         if(Arrays.deepEquals(answerKey,isCorrectS)) {
             TextView textview = (TextView) getActivity().findViewById(R.id.scoreText);
             textview.setText("WIN");
             gameOver = true;
-        }
+            finalScore = fragScore;
 
-        //BELOW IS WHERE THE CODE FOR SCORE STORAGE CAN GO...
+            loadData();
+            if(fragScore > hs3 || fragScore == hs3) {
+                requestHighscore();
+            } else {
+                displayEndPopup();
+                for(String s: hsNames) {
+                    System.out.println(s);
+                }
+            }
 
+        } // end deepEquals
     }
+
+
+    private void saveData() {
+        int finalScore = fragScore;
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences(getSharedPrefs(numOfCards), MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("score", finalScore);
+        editor.apply();
+    }
+
+    private void loadData() {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences(getSharedPrefs(numOfCards), MODE_PRIVATE);
+        lastScore = sharedPreferences.getInt("score", 0);
+        lastName = sharedPreferences.getString("name", GameFragment.userName);
+        hs1 = sharedPreferences.getInt("hs1", 0);
+        hs2 = sharedPreferences.getInt("hs2", 0);
+        hs3 = sharedPreferences.getInt("hs3", 0);
+        ns1 = sharedPreferences.getString("ns1", "ABC");
+        ns2 = sharedPreferences.getString("ns2", "ABC");
+        ns3 = sharedPreferences.getString("ns3", "ABC");
+        scoreReadout = sharedPreferences.getString("scoreString", scoreReadout);
+    }
+
+    private String getSharedPrefs(int n) {
+        n = numOfCards;
+        if(n == 4) {
+            return "sharedPrefs4";
+        }
+        if(n == 6) {
+            return "sharedPrefs6";
+        }
+        if(n == 8) {
+            return "sharedPrefs8";
+        }
+        if(n == 10) {
+            return "sharedPrefs10";
+        }
+        if(n == 12) {
+            return "sharedPrefs12";
+        }
+        if(n == 14) {
+            return "sharedPrefs14";
+        }
+        if(n == 16) {
+            return "sharedPrefs16";
+        }
+        if(n == 18) {
+            return "sharedPrefs18";
+        }
+        if(n == 20) {
+            return "sharedPrefs20";
+        }
+        return"";
+    }
+
+    private void requestHighscore() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+        alert.setMessage("Please enter your initials");
+        final EditText input = new EditText(getContext());
+        alert.setView(input);
+        alert.setCancelable(false);
+        alert.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+
+                saveData();
+
+                userName = input.getText().toString();
+
+
+                SharedPreferences sharedPreferences = getActivity().getSharedPreferences(getSharedPrefs(numOfCards), MODE_PRIVATE);
+                loadData();
+
+                Integer[] hsArr = {hs1, hs2, hs3};
+                int min = Collections.min(Arrays.asList(hsArr));
+                int max = Collections.max(Arrays.asList(hsArr));
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                if(lastScore > hs1 || lastScore == hs1) {
+                    //Old high score 2 becomes high score 3
+                    int oldHs2 = hs2;
+                    String oldNs2 = ns2;
+
+                    hs3 = oldHs2;
+                    ns3 = oldNs2;
+
+                    //Old high score 1 becomes high score 2
+                    int oldHs1 = hs1;
+                    String oldNs1 = ns1;
+
+                    hs2 = oldHs1;
+                    ns2 = oldNs1;
+
+                    //Old high score 1 is replaced by latest user input
+                    hs1 = lastScore;
+                    ns1 = lastName;
+
+
+                    editor.putInt("hs1", hs1);
+                    editor.putInt("hs2", hs2);
+                    editor.putInt("hs3", hs3);
+                    editor.putString("ns1", ns1);
+                    editor.putString("ns2", ns2);
+                    editor.putString("ns3", ns3);
+                    editor.apply();
+                }
+                else if(lastScore < hs1 && lastScore >= hs2) {
+
+                    //Old high score 2 becomes high score 3
+                    int oldHs2 = hs2;
+                    String oldNs2 = ns2;
+
+                    hs3 = oldHs2;
+                    ns3 = oldNs2;
+
+                    //Old high score 1 is replaced by latest user input
+                    hs2 = lastScore;
+                    ns2 = lastName;
+
+                    editor.putInt("hs2", hs2);
+                    editor.putInt("hs3", hs3);
+                    editor.putString("ns2", ns2);
+                    editor.putString("ns3", ns3);
+                    editor.apply();
+                }
+                else if(lastScore < hs2 && lastScore >= hs3) {
+                    hs3 = lastScore;
+                    ns3 = lastName;
+
+                    editor.putInt("hs3", hs3);
+                    editor.putString("ns3", ns3);
+                    editor.apply();
+                }
+
+
+                scoreReadout =  "\n# of Cards: " + numOfCards + "\n" +
+                                "Last Score: " + lastScore + "\n" +
+                                "-------------------------" + "\n" +
+                                ns1 + "\t : \t " + hs1 + "\n" +
+                                ns2 + "\t : \t " + hs2 + "\n" +
+                                ns3 + "\t : \t " + hs3;
+                editor.putString("scoreString", scoreReadout);
+                editor.apply();
+
+            }
+        });
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                dialog.cancel();
+            }
+        });
+        alert.show();
+    }
+
+    private void displayEndPopup() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+        alert.setMessage("GAME OVER!\n" + "Score: " + fragScore);
+        alert.setNegativeButton("End", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                dialog.cancel();
+            }
+        });
+        alert.show();
+    }
+
 
     private void disableButtons() {
         for(int i = 0; i < m_max; i++) {
--- app/src/main/java/com/monemobility/monememory/MainActivity.java
+++ app/src/main/java/com/monemobility/monememory/MainActivity.java
@@ -1,17 +1,37 @@
 package com.monemobility.monememory;
 
+import static java.security.AccessController.getContext;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class MainActivity extends AppCompatActivity {
     int length;
@@ -25,11 +45,19 @@ public class MainActivity extends AppCompatActivity {
     Button scoreButton;
     SeekBar difficultyBar;
     GameFragment gameFragment;
-    public int playerScore = 1;
+    TextView tv;
+    public int playerScore = 0;
+
     Boolean musicState;
     ImageView[][] IVArray= new ImageView[length][width];
 
     Button muteButton;
+
+    int lastScore, hs1, hs2, hs3;
+    String lastName, ns1, ns2, ns3;
+    String scoreReadout;
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String SCORE = "4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,25 +112,32 @@ public class MainActivity extends AppCompatActivity {
 
         scoreButton = this.findViewById(R.id.scoreText);
         scoreButton.setOnClickListener(new View.OnClickListener() {
+            String output = "";
             @Override
             public void onClick(View view) {
+                AlertDialog.Builder scoreAlert = new AlertDialog.Builder((MainActivity.this));
 
+                tv = loadData();
+
+
+                scoreAlert.setView(tv);
+
+
+
+                scoreAlert.setPositiveButton("Clear", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        SharedPreferences sharedPreferences = getSharedPreferences(getSharedPrefs(difficulty), MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.clear();
+                        editor.apply();
+                        Toast.makeText(MainActivity.this, "High scores for this game have been cleared.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                scoreAlert.show();
             }
         });
 
-
-        /*
-        Bundle bundle = new Bundle();
-        bundle.putString("edttext", Integer.toString(4));
-        // set Fragmentclass Arguments
-        gameFragment = new GameFragment();
-        gameFragment.setArguments(bundle);
-
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.game_container, gameFragment)
-                .addToBackStack(GameFragment.class.getName()).commit();
-
-*/
         if (savedInstanceState == null) {
             Bundle bundle = new Bundle();
             bundle.putString("edttext", Integer.toString(4));
@@ -114,13 +149,51 @@ public class MainActivity extends AppCompatActivity {
                     .addToBackStack(GameFragment.class.getName()).commit();
 
         }
-
+        Toast.makeText(this, "Press on the Score to see the high scores for this game.", Toast.LENGTH_SHORT).show();
     }
+
+
+
+
+
+    private String getSharedPrefs(int n) {
+        n = difficulty;
+        if(n == 4) {
+            return "sharedPrefs4";
+        }
+        if(n == 6) {
+            return "sharedPrefs6";
+        }
+        if(n == 8) {
+            return "sharedPrefs8";
+        }
+        if(n == 10) {
+            return "sharedPrefs10";
+        }
+        if(n == 12) {
+            return "sharedPrefs12";
+        }
+        if(n == 14) {
+            return "sharedPrefs14";
+        }
+        if(n == 16) {
+            return "sharedPrefs16";
+        }
+        if(n == 18) {
+            return "sharedPrefs18";
+        }
+        if(n == 20) {
+            return "sharedPrefs20";
+        }
+        return"";
+    }
+
+
     @Override
     protected void onDestroy() {
-            super.onDestroy();
-            Intent intent = new Intent(getApplicationContext(), BackgroundMusicService.class);
-            //stopService(intent);
+        super.onDestroy();
+        Intent intent = new Intent(getApplicationContext(), BackgroundMusicService.class);
+        //stopService(intent);
     }
 
     @Override
@@ -149,6 +222,8 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
         String savedTextScore = (String) scoreButton.getText();
         String savedTextMutePlay = (String) muteButton.getText();
+        int savedDifficulty = difficulty;
+        outState.putInt("sD", savedDifficulty);
         outState.putString("s", savedTextScore);
         outState.putString("mp", savedTextMutePlay);
         outState.putBoolean("m", musicState);
@@ -159,6 +234,7 @@ public class MainActivity extends AppCompatActivity {
         String savedText = (String) scoreButton.getText();
         String x = savedInstanceState.getString("s");
         String y = savedInstanceState.getString("mp");
+        difficulty = savedInstanceState.getInt("sD");
         musicState = savedInstanceState.getBoolean("m");
         scoreButton.setText(x);
         muteButton.setText(y);
@@ -189,7 +265,7 @@ public class MainActivity extends AppCompatActivity {
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                playerScore = 1;
+                playerScore = 0;
                 scoreButton.setText("SCORE: " + playerScore);
                 Bundle bundle = new Bundle();
                 bundle.putString("edttext", Integer.toString(difficulty));
@@ -233,6 +309,35 @@ public class MainActivity extends AppCompatActivity {
         scoreButton.setText("SCORE: " + playerScore);
     }
 
+    private TextView loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(getSharedPrefs(difficulty), MODE_PRIVATE);
+        lastScore = sharedPreferences.getInt("score", 0);
+        lastName = sharedPreferences.getString("name", GameFragment.userName);
+        hs1 = sharedPreferences.getInt("hs1", 0);
+        hs2 = sharedPreferences.getInt("hs2", 0);
+        hs3 = sharedPreferences.getInt("hs3", 0);
+        ns1 = sharedPreferences.getString("ns1", "ABC");
+        ns2 = sharedPreferences.getString("ns2", "ABC");
+        ns3 = sharedPreferences.getString("ns3", "ABC");
+        scoreReadout =  "\n# of Cards: " + difficulty + "\n" +
+                        "Last Score: " + lastScore + "\n" +
+                        "-------------------------" + "\n" +
+                        ns1 + "\t : \t " + hs1 + "\n" +
+                        ns2 + "\t : \t " + hs2 + "\n" +
+                        ns3 + "\t : \t " + hs3;
+        scoreReadout = sharedPreferences.getString("scoreString", scoreReadout);
+        TextView tv = new TextView(this);
+        tv.setText(scoreReadout);
+        tv.setGravity(Gravity.CENTER_HORIZONTAL);
+
+        return tv;
+    }
+
+
+    public void onBackPressed() {
+
+    }
+
+
 
 }
-
